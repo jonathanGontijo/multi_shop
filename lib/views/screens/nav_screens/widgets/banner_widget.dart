9f44953b93d244bac61bf6c890fcf228b8574e88
Jonathan Gontijo <jonathan.ferreira1312@hotmail.com>
2024-05-16 10:11:40 -0300

    banner widget

---
+++ lib/views/screens/nav_screens/widgets/banner_widget.dart
@@ -0,0 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class BannerWidget extends StatefulWidget {
+  const BannerWidget({super.key});
+
+  @override
+  State<BannerWidget> createState() => _BannerWidgetState();
+}
+
+class _BannerWidgetState extends State<BannerWidget> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  final List _bannerImage = [];
+
+  getBanners() {
+    return _firestore
+        .collection('banners')
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        setState(() {
+          _bannerImage.add(doc['image']);
+        });
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    getBanners();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 140,
+      width: MediaQuery.of(context).size.width,
+      child: PageView.builder(
+        itemCount: _bannerImage.length,
+        itemBuilder: (context, index) {
+          return Image.network(_bannerImage[index]);
+        },
+      ),
+    );
+  }
+}
