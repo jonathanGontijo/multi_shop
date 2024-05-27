5ef6d1b5fddc3f94e907d3cafcb6b1385018ca62
jonathanGontijo <jonathan.ferreira1312@hotmail.com>
2024-05-27 19:01:14 -0300

    bottom sheet

--- lib/views/screens/inner_screens/product_detail_screen.dart
+++ lib/views/screens/inner_screens/product_detail_screen.dart
@@ -36,63 +36,185 @@ class ProductDetailScreen extends StatelessWidget {
         ),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            width: 260,
-            height: 274,
-            clipBehavior: Clip.hardEdge,
-            decoration: const BoxDecoration(),
-            child: Stack(
-              clipBehavior: Clip.none,
-              children: [
-                Positioned(
-                  left: 0,
-                  top: 0,
-                  child: Container(
-                    width: 260,
-                    height: 260,
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      color: const Color(
-                        0xFFD8DDFF,
+          Center(
+            child: Container(
+              width: 260,
+              height: 274,
+              clipBehavior: Clip.hardEdge,
+              decoration: const BoxDecoration(),
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Positioned(
+                    left: 0,
+                    top: 0,
+                    child: Container(
+                      width: 260,
+                      height: 260,
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        color: const Color(
+                          0xFFD8DDFF,
+                        ),
+                        borderRadius: BorderRadius.circular(130),
                       ),
-                      borderRadius: BorderRadius.circular(130),
+                    ),
+                  ),
+                  Positioned(
+                    left: 22,
+                    top: 0,
+                    child: Container(
+                      width: 216,
+                      height: 274,
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        color: const Color(
+                          0xFF9CA8FF,
+                        ),
+                        borderRadius: BorderRadius.circular(14),
+                      ),
+                      child: SizedBox(
+                        height: 300,
+                        child: PageView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: productData['productImage'].length,
+                            itemBuilder: (context, index) {
+                              return Image.network(
+                                productData['productImage'][index],
+                                width: 198,
+                                height: 225,
+                                fit: BoxFit.cover,
+                              );
+                            }),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  productData['productName'],
+                  style: GoogleFonts.roboto(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1,
+                    color: const Color(
+                      0xFF3C55EF,
                     ),
                   ),
                 ),
-                Positioned(
-                  left: 22,
-                  top: 0,
-                  child: Container(
-                    width: 216,
-                    height: 274,
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      color: const Color(
-                        0xFF9CA8FF,
-                      ),
-                      borderRadius: BorderRadius.circular(14),
+                Text("\$${productData['productPrice'].toStringAsFixed(2)}")
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              productData['category'],
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Size:',
+                  style: GoogleFonts.lato(
+                    color: const Color(
+                      0xFF343434,
                     ),
-                    child: SizedBox(
-                      height: 300,
-                      child: PageView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: productData['productImage'].length,
-                          itemBuilder: (context, index) {
-                            return Image.network(
-                              productData['productImage'][index],
-                              width: 198,
-                              height: 225,
-                              fit: BoxFit.cover,
-                            );
-                          }),
-                    ),
+                    fontSize: 16,
+                    letterSpacing: 1.6,
                   ),
+                ),
+                SizedBox(
+                  height: 50,
+                  child: ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: productData['productSize'].length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: InkWell(
+                            onTap: () {},
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFF126881),
+                                  borderRadius: BorderRadius.circular(5)),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: Text(
+                                  productData['productSize'][index],
+                                  style: GoogleFonts.lato(color: Colors.white),
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'About',
+                  style: GoogleFonts.lato(
+                    color: const Color(0xFF363330),
+                    fontSize: 16,
+                    letterSpacing: 1,
+                  ),
+                ),
+                Text(
+                  productData['description'],
                 ),
               ],
             ),
-          )
+          ),
         ],
+      ),
+      bottomSheet: Padding(
+        padding: const EdgeInsets.all(8),
+        child: InkWell(
+          onTap: () {},
+          child: Container(
+            width: 386,
+            height: 48,
+            clipBehavior: Clip.antiAlias,
+            decoration: BoxDecoration(
+              color: const Color(0xFF3B54EE),
+              borderRadius: BorderRadius.circular(24),
+            ),
+            child: Center(
+              child: Text(
+                'ADD TO CART',
+                style: GoogleFonts.lato(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
