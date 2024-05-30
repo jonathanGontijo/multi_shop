de524c1b40017658c7bb76e8c7e3cac3551f9518
jonathanGontijo <jonathan.ferreira1312@hotmail.com>
2024-05-30 19:05:08 -0300

    favorite model and provider

---
+++ lib/provider/favorite_provider.dart
@@ -0,0 +1,50 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:multi_shop/models/favorite_models.dart';
+
+final favoriteProvider =
+    StateNotifierProvider<FavoriteNotifier, Map<String, FavoriteModel>>(
+  (ref) {
+    return FavoriteNotifier();
+  },
+);
+
+class FavoriteNotifier extends StateNotifier<Map<String, FavoriteModel>> {
+  FavoriteNotifier() : super({});
+
+  //is to add product to favorite
+
+  void addProductToFavorite({
+    required String productName,
+    required String productId,
+    required List imageUrl,
+    required int productPrice,
+  }) {
+    state[productId] = FavoriteModel(
+        productName: productName,
+        productId: productId,
+        imageUrl: imageUrl,
+        productPrice: productPrice);
+
+    // notify listeners that the state has changed
+
+    state = {...state};
+  }
+
+  //remove all item from favorite
+
+  void removeAllItem() {
+    state.clear();
+
+    // notify listeners that the state has changed
+
+    state = {...state};
+  }
+
+  //remove favorite item
+  void removeItem(String productId) {
+    state.remove(productId);
+    // notify listeners that the state has changed
+
+    state = {...state};
+  }
+}
